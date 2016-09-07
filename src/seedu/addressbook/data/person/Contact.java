4dc45ebee84191f66192267ef1c1ea56516074c5
Desmond <e0012764@u.nus.edu>
2016-09-07 15:35:58 +0800

    Created Contact class as parent class and edited Address, Email and Phone class to inherit from Contact class

---
+++ src/seedu/addressbook/data/person/Contact.java
@@ -0,0 +1,11 @@
+package seedu.addressbook.data.person;
+
+public class Contact {
+
+    protected boolean isPrivate;
+    
+    public boolean isPrivate() {
+        return isPrivate;
+    }
+    
+}
